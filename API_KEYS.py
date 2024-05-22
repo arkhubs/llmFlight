99b81dc8acba0fa7573a6ae9cc6c52c4bf08125e
arkhubs <1459826385@qq.com>
2024-05-22 12:18:58 +0800

    test

--- API_KEYS.py
+++ API_KEYS.py
@@ -2,11 +2,11 @@ import json
 
 API_KEYS = {
     "text-embedding-3-small": {
-        "api_key": "",   # Please fill in your own api_key 
+        "api_key": "11111111111", 
         "base_url": "https://api.crowtalk.live/v1"
     }, 
     "gpt-3.5-turbo-0125": {
-        "api_key": "",   # Please fill in your own api_key  
+        "api_key": "11111111111", 
         "base_url": "https://api.crowtalk.live/v1"
     }
 }
